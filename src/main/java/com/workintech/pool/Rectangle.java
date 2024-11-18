78ff716ea93ee41fb4c374b45e963ccb32a333c6
replakcan <alper.makcan@gmail.com>
2024-11-18 16:50:55 +0300

    creates class Rectangle under .pool package

---
+++ src/main/java/com/workintech/pool/Rectangle.java
@@ -0,0 +1,19 @@
+package com.workintech.pool;
+
+public class Rectangle {
+
+    private float width;
+    private float length;
+
+    public float getWidth() {
+        return width;
+    }
+
+    public float getLength() {
+        return length;
+    }
+
+    public float getArea() {
+        return this.width * this.length;
+    }
+}
