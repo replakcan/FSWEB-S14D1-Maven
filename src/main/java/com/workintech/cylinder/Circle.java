d47d5242c86da8fe67f3d0525818d5b89f404ae1
replakcan <alper.makcan@gmail.com>
2024-11-18 16:38:31 +0300

    creates class Circle under new created Package

---
+++ src/main/java/com/workintech/cylinder/Circle.java
@@ -0,0 +1,18 @@
+package com.workintech.cylinder;
+
+public class Circle {
+
+    private double radius;
+
+    public Circle (double radius){
+        this.radius = Math.max(radius, 0);
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return this.radius * this.radius * Math.PI;
+    }
+}
